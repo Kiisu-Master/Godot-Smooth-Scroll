4f2bd0ae9675b04708bf4b709b6a1a89fe34bae5
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2024-02-04 14:16:30 +0200

    Update smooth_scroll.gd

--- addons/smooth_scroll/smooth_scroll.gd
+++ addons/smooth_scroll/smooth_scroll.gd
@@ -9,29 +9,28 @@ class_name SmoothScroll extends Control
 @export var scroll_step: float = 1.0
 
 var _last_pos: Vector2  # The last known mouse position.
-var _last_pos_glob: Vector2
+var _last_pos_global: Vector2
 var _needed_scroll: Vector2  # The scroll amount.
 var _last_event: InputEventMouseButton  # Last event to copy properties from.
 
 @onready var _window := get_window()
 
 
-func _ready() -> void:
-	process_mode = Node.PROCESS_MODE_ALWAYS  # Don't stop smooth scroll when paused.
+func _enter_tree() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		_last_pos = event.position  # Update position if mouse moves.
-		_last_pos_glob = event.global_position  # Just in case.
+		# Update position on mouse motion.
+		_last_pos = event.position
+		_last_pos_global = event.global_position
 	
 	elif event is InputEventMouseButton:
-		
 		# Only intercept scrolling events.
-		if not event.pressed or not event.button_index in \
-			[MOUSE_BUTTON_WHEEL_DOWN, MOUSE_BUTTON_WHEEL_UP,
-			MOUSE_BUTTON_WHEEL_RIGHT, MOUSE_BUTTON_WHEEL_LEFT]:
-				return
+		if not event.pressed or not event.button_index in [MOUSE_BUTTON_WHEEL_DOWN,
+		MOUSE_BUTTON_WHEEL_UP, MOUSE_BUTTON_WHEEL_RIGHT, MOUSE_BUTTON_WHEEL_LEFT]:
+			return
 		
 		# Detect if input doesn't have smooth factor.
 		# That is when the factor is a multiple of 1, an integer.
@@ -41,7 +40,7 @@ func _input(event: InputEvent) -> void:
 			
 			# Calculate the step factor.
 			# The event factor can be higher than 1, which has to be taken into account.
-			var factor: float = scroll_step * max(event.factor, 1.0)
+			var factor := scroll_step * maxf(event.factor, 1.0)
 			
 			match event.button_index:
 				MOUSE_BUTTON_WHEEL_DOWN:
@@ -61,12 +60,12 @@ func _process(delta: float) -> void:
 	var event := _last_event  # Copy properties of last scroll event.
 	
 	# Calculate distance to scroll on this frame.
-	var dist: Vector2 = _needed_scroll * min(delta * scroll_speed, 0.99)  # Limit to 0.99 to prevent overshoot and infinite feedback on low FPS.
+	# Limit to 0.99 to prevent overshoot and infinite feedback on low FPS.
+	var dist := _needed_scroll * minf(delta * scroll_speed, 0.99)
 	var abs_dist := dist.abs()
 	
-	# Determine direction.
-	if abs_dist.x < abs_dist.y:  # Vertical scroll.
-		
+	if abs_dist.x < abs_dist.y:
+		# Vertical scroll.
 		if dist.y > 0:
 			event.button_index = MOUSE_BUTTON_WHEEL_DOWN
 			event.button_mask = 0b10000  # Magic mask value found in actual scrollwheel input event.
@@ -74,11 +73,11 @@ func _process(delta: float) -> void:
 			event.button_index = MOUSE_BUTTON_WHEEL_UP
 			event.button_mask = 0b01000
 		
-		_needed_scroll.y -= dist.y  # Decrease needed scroll amount by the amount scrolled.
+		# Decrease needed scroll amount by the amount scrolled.
+		_needed_scroll.y -= dist.y
 		event.factor = abs_dist.y
-	
-	else:  # Horizontal scroll.
-		
+	else:
+		# Horizontal scroll.
 		if dist.x > 0:
 			event.button_index = MOUSE_BUTTON_WHEEL_RIGHT
 			event.button_mask = 0b10000  # 16
@@ -91,13 +90,11 @@ func _process(delta: float) -> void:
 	
 	# Apply common data to event.
 	event.position = _last_pos * _window.content_scale_factor
-	event.global_position = _last_pos_glob
+	event.global_position = _last_pos_global
 	
 	# Send fake input event with factor.
 	Input.parse_input_event(event)
-	
-	event = event.duplicate()  # This is the most expensive thing. ( 3/4 of the computation time)
-	
+	event = event.duplicate()
 	# Disable pressed state.
 	event.button_mask = 0
 	event.pressed = false
